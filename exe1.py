7e60ac7e326fc469d0f74892f977a421bd749908
semosKG <samuelkumelag@gmail.com>
2025-10-18 10:09:57 +0300

    exeersice

--- exe1.py
+++ exe1.py
@@ -4,3 +4,8 @@ def add (a,b):
 
 
 print(" result:", add(2, 3))
+// git clone https://github.com/Semoskg/execrise1.git
+// git add exe1.py
+// git commit -m "initial commit"
+// git push origin main
+/git pull origin main
